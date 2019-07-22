1a4eadf05d2c7d2bd13e3033f42d53aa9f4e408d
Stuart Wong <stuartmrwong@gmail.com>
2019-07-22 12:16:55 -0700

    Add files via upload
    Added ability to delete comments from all lines
    Replace all comments with 30 random characters to create noise
    Retain all code
    Retain any and all shebangs

--- ScriptAugment.py
+++ ScriptAugment.py
@@ -12,9 +12,32 @@ read the file and write with the intent to obscure authorship
 
 import os
 import shutil
+import re
+import random
+import string
+from collections import OrderedDict
 
+def remove_comment(line, finish):
+    line = line[0: finish] #remove the comment by slicing the string with the index of the start to the #
+    return line
+                  
+def add_comment():
+    line = str('#')
+    return line
 
-
+def create_noise(length, chars):
+    return '#' + ''.join(random.choices(chars, k=length)) + '\n'
+        
+def add_remove_line(line):
+    d20 = random.randint(1,20) #random integer between 1 and 20, not crypto secure
+    if d20 <= 2: #20% chance to add a line
+        line = line + '\n'
+    if d20 >=19: #20% chance to strip the line
+        line = line.strip()
+    else:
+        pass
+    return line
+    
 def file_length(filename): #this function will calculate the number of lines
     with open(filename) as f: #open the file and call it f
         for i, l in enumerate(f): #start a for loop that counts each line
@@ -39,6 +62,8 @@ if os.path.exists(Truefile): #check that the file 'a' actually exists
     c = open(Editfile,'r')#open the new file in read only mode
     Oldcode = c.read()
     #print('This is the old code: \n\n\n' + Oldcode)
+    c.close()
+    Newcode = '' #this will hold our new, obfuscated code
     
     quarter = Totallines / 4
     quarter = int(quarter)
@@ -55,28 +80,81 @@ if os.path.exists(Truefile): #check that the file 'a' actually exists
     print('\n')
     
     """ this prints each line with a line number"""
-    Sex = {} #define a empty dictionary as Sex
+    CodeDict = {} #define a empty dictionary as CodeDict
     with open(Editfile) as Oc: #open the new file
         for count, line in enumerate(Oc): #for loop that iterates through each
             #print("Line {}: {}".format(count, line)) #print all the code with line numbers
-            Sex.setdefault(count, line) #add each line as key and code as value to the empty dictionary Sex
-    #print(Sex) #print Sex just so I can see that it works
-    #print(Sex[1]) #proving I can call specific key value pairs
+            CodeDict.setdefault(count, line) #add each line as key and code as value to the empty dictionary CodeDict
+    #print(CodeDict) #print CodeDict just so I can see that it works
+    #print(CodeDict[1]) #proving I can call specific key value pairs
     
-    for i in range(1, quarter): #printing the first block
-        print(Sex[i])
+    #START BLOCK 1
+    #scanning through the first block(25% of the code)
+    for i in range(0, threequarter): 
         
-        if '#' in Sex[i] == True:
-            #function to replace comments here
-            pass
+        #if you find a shebang, dont do anything 
+        if CodeDict[i].find('#!') >= 0: 
+            print('Shebang found')
+            Newcode += CodeDict.get(i)
+            pass           
+        
+        #if we find a comment, delete the comment, write a new comment
+        elif CodeDict[i].find('#') >= 0: 
+            CommentLocation = CodeDict[i].find('#')                    #acquire the location of the #                    
+            print('Comment Detected in line ' + str(i) + ' at character ' + str(CommentLocation))
+            CodeDict[i] = remove_comment(CodeDict[i], CommentLocation) #slice out the comment and leave anything else
+            print('Creating new line: ' + str(CodeDict.get(i)))        #get the new, sliced line                  
+            Newcode += CodeDict.get(i) + str(create_noise(30, string.ascii_letters + string.digits + string.punctuation))            #add a comment back in because there was one before
+        
+        #if we find an empty line, randomly add another or delete it
+        elif '\n' in CodeDict[i] == True:
+            print('\n>>>new line detected. calculating to add or delete')
+            CodeDict[i] = add_remove_line(CodeDict[i])
+            print(CodeDict[i])
+            
+        #if its something we dont recognize, leave it to ensure functionality
         else:
+            print(CodeDict[i])
+            Newcode += CodeDict.get(i)
             pass
+            
     print('END OF BLOCK 1')
-    
+    print('New code: ')
+    print(Newcode)
+    #END OF BLOCK 1
+    """
+    #scanning through the second block(25% - 50% of the code)
+    for i in range(quarter, half): 
+               
+        #if we find a comment, delete the comment, write a new comment
+        if CodeDict[i].find('#') >= 0: 
+            CommentLocation = CodeDict[i].find('#')                    #acquire the location of the #                    
+            print('Comment Detected in line ' + str(i) + ' at character ' + str(CommentLocation))
+            CodeDict[i] = remove_comment(CodeDict[i], CommentLocation) #slice out the comment and leave anything else
+            print('Creating new line: ' + str(CodeDict.get(i)))        #get the new, sliced line                  
+            Newcode += CodeDict.get(i) + str(create_noise(30, string.ascii_letters + string.digits + string.punctuation))            #add a comment back in because there was one before
+            
+        elif '\n' in CodeDict[i] == True:
+            print('\n>>>new line detected. calculating to add or delete')
+            CodeDict[i] = add_remove_line(CodeDict[i])
+            print(CodeDict[i])
+        
+        else:
+            print(CodeDict[i])
+            pass
+            
+    print('END OF BLOCK 2')
+    print('New code: ')
+    print(Newcode)
+    #END OF BLOCK 2
+    """
+    with open(Editfile, 'w') as Nc:
+        Nc.write(Newcode)
+        
     
     """
     for i in range(quarter, half): #printing the second block
-        print(Sex[i])
+        print(CodeDict[i])
     """
     
     
